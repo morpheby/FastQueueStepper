27ed71fec0b6bf08688f8301cc05861f07f3e752
Ilya Mikhaltsou <morpheby@gmail.com>
2025-02-26 23:55:13 +0300

    fixup! Don’t copy-paste stuff without reading

--- src/FastQueueStepper_idf5_esp32_pcnt.cpp
+++ src/FastQueueStepper_idf5_esp32_pcnt.cpp
@@ -80,12 +80,12 @@ bool FastQueueStepper::enablePulseCounter() {
     return false;
   }
 
-  rc = pcnt_unit_add_watch_point(pcnt_unit, INT16_MAX);
+  rc = pcnt_unit_add_watch_point(punit, INT16_MAX);
   if (rc != ESP_OK) {
     return false;
   }
 
-  rc = pcnt_unit_add_watch_point(pcnt_unit, INT16_MIN);
+  rc = pcnt_unit_add_watch_point(punit, INT16_MIN);
   if (rc != ESP_OK) {
     return false;
   }
